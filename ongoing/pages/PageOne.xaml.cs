75a24629b38471a7a30f479101fce9a4a89e57f4
yuvi4444 <83678519+yuvi4444@users.noreply.github.com>
2024-06-11 23:12:08 +1000

    added homepage

--- ongoing/pages/PageOne.xaml.cs
+++ ongoing/pages/PageOne.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -17,9 +17,15 @@ namespace ongoing.pages
             InitializeComponent();
         }
 
-        private void Button_Clicked(object sender, EventArgs e)
+        private void New_User_Button_Clicked(object sender, EventArgs e)
         {
             DisplayAlert("helo", "you clicked!", "bye");
         }
+
+        private void Existing_User_Button_Clicked(Object sender, EventArgs e)
+        {
+            DisplayAlert("helo", "you clicked!", "bye");
+        }
+
     }
 }
